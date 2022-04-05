0624184d854ab9ee09d26b91b16cfc430b8f610b
Honggang Zhuang <honggang@usc.edu>
2022-04-04 23:24:46 -0700

    modify Assignment2.md

--- homework/Assignment2.md
+++ homework/Assignment2.md
@@ -18,6 +18,9 @@
 | long                   | 0l             |
 | Object                 | null           |
 | char                   | \u0000         |
+| int                    | 0              |
+| double                 | 0.0d           |
+| boolean                | false          |
 
 ### 6. What is garbage collection?
 * Java garbage collection is the process by which Java programs perform automatic memory management. Java programs compile to bytecode that can be run on a Java Virtual Machine, or JVM for short. When Java programs run on the JVM, objects are created on the heap, which is a portion of memory dedicated to the program.
@@ -43,7 +46,7 @@ Autoboxing is the automatic conversion that the Java compiler makes between the 
 
 ### 12. StringBuilder is threadsafe but slower than StringBuffer, true or false?
 * False
-* 
+
 ### 13. Constructor can be inherited, true or false?
 * False
 
--- src/main/java/Assignment2/FileCounter.java
+++ src/main/java/Assignment2/FileCounter.java
@@ -1,12 +1,38 @@
 package Assignment2;
 
-public class FileCounter {
+import java.io.File;
 
+public class FileCounter {
+    public static void main(String[] args) {
+        return;
+    }
+
+    public static void count(Criteria criteria) {
+        File directory = new File(criteria.getFolderPath());
+
+    }
 }
 
 class Criteria {
     private String folderPath;
     private boolean containSubFolder;
+    private String fileExtension;
 
+    public Criteria(String folderPath, boolean containSubFolder, String fileExtension) {
+        this.folderPath = folderPath;
+        this.containSubFolder = containSubFolder;
+        this.fileExtension = fileExtension;
+    }
 
+    public String getFolderPath() {
+        return folderPath;
+    }
+
+    public boolean isContainSubFolder() {
+        return containSubFolder;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
 }
