6b9a9b3af6d2b5c0992013fb66647f25fe3d35f3
Honggang Zhuang <honggang@usc.edu>
2022-04-09 23:40:13 -0700

    modified Assignment 4 question 15

---
+++ src/main/java/Assignment4/ThreadPractice.java
@@ -0,0 +1,13 @@
+package Assignment4;
+
+public class ThreadPractice {
+    /*
+    15. Create a program including two threads – thread read and thread write.
+    Input file ->Thread read -> Calculate -> buffered area Buffered area -> Thread write -> output file
+    Detailed description is in assignment4.txt file. Sample input.txt file
+    Attached files are input.txt and a more detailed description file.
+     */
+    public static void main(String[] args) {
+
+    }
+}
